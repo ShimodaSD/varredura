355c6bc39a9a53e06502a69ee25093f186418ff5
11811758@acad.espm.br <11811758@acad.espm.br>
2019-10-23 12:19:21 -0300

    First commit

---
+++ Varredura/FormMain.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace HtmlDesktop
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.colNotifica = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colData = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colTitulo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colLink = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(12, 12);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(89, 33);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Atualizar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // listView
+            // 
+            this.listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNotifica,
+            this.colData,
+            this.colTitulo,
+            this.colLink});
+            this.listView.FullRowSelect = true;
+            this.listView.GridLines = true;
+            this.listView.Location = new System.Drawing.Point(12, 51);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(776, 387);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.DoubleClick += new System.EventHandler(this.listView_DoubleClick);
+            // 
+            // colNotifica
+            // 
+            this.colNotifica.Text = "Atenção";
+            this.colNotifica.Width = 100;
+            // 
+            // colData
+            // 
+            this.colData.Text = "Data";
+            this.colData.Width = 125;
+            // 
+            // colTitulo
+            // 
+            this.colTitulo.Text = "Título";
+            this.colTitulo.Width = 300;
+            // 
+            // colLink
+            // 
+            this.colLink.Text = "Link";
+            this.colLink.Width = 100;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Nova notícia!!!";
+            this.notifyIcon.BalloonTipTitle = "Monitorador";
+            this.notifyIcon.Text = "Monitorador";
+            this.notifyIcon.BalloonTipClicked += new System.EventHandler(this.notifyIcon_BalloonTipClicked);
+            this.notifyIcon.Click += new System.EventHandler(this.notifyIcon_Click);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 120000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.btnBuscar);
+            this.Name = "FormMain";
+            this.Text = "Verify";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader colData;
+        private System.Windows.Forms.ColumnHeader colTitulo;
+        private System.Windows.Forms.ColumnHeader colLink;
+        private System.Windows.Forms.ColumnHeader colNotifica;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Varredura/FormMain.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HtmlDesktop
+{
+    public partial class FormMain : Form
+    {
+        public class Noticia
+        {
+            public long Id { get; set; }
+            public string Notificacao { get; set; }
+            public string DataStr { get; set; }
+            public string Titulo { get; set; }
+            public string Link { get; set; }
+            public DateTime Data { get; set; }
+            public bool Analisar { get; set; }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHELLEXECUTEINFO
+        {
+            public int cbSize;
+            public uint fMask;
+            public IntPtr hwnd;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpVerb;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpFile;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpParameters;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpDirectory;
+            public int nShow;
+            public IntPtr hInstApp;
+            public IntPtr lpIDList;
+            [MarshalAs(UnmanagedType.LPTStr)]
+            public string lpClass;
+            public IntPtr hkeyClass;
+            public uint dwHotKey;
+            public IntPtr hIcon;
+            public IntPtr hProcess;
+        }
+
+        [DllImport("shell32.dll", CharSet = CharSet.Auto)]
+        private static extern bool ShellExecuteEx(ref SHELLEXECUTEINFO lpExecInfo);
+
+        private List<Noticia> noticias;
+        private string banco;
+
+        private int ParseLinha(string html, int inicio, List<Noticia> lista)
+        {
+            int fimLinha = html.IndexOf("</tr>", inicio);
+            if (fimLinha < 0)
+                return -1;
+
+            int fim = html.IndexOf("</td>", inicio);
+            if (fim < 0 || fim > fimLinha)
+                return -1;
+
+            string data = html.Substring(inicio, fim - inicio).Trim();
+
+            int classA = html.IndexOf("class=\"titulo_noticias\"", inicio);
+            if (classA < 0 || classA > fimLinha)
+                return -1;
+
+            int href = html.IndexOf("href=\"", classA);
+            if (href < 0 || href > fimLinha)
+                return -1;
+            href += 6;
+
+            int fimHref = html.IndexOf('\"', href);
+            if (fimHref < 0 || fimHref > fimLinha)
+                return -1;
+
+            string link = html.Substring(href, fimHref - href).Trim();
+
+            int inicioTitulo = html.IndexOf('>', fimHref);
+            if (inicioTitulo < 0 || inicioTitulo > fimLinha)
+                return -1;
+            inicioTitulo++;
+
+            int fimTitulo = html.IndexOf('<', inicioTitulo);
+            if (fimTitulo < 0 || fimTitulo > fimLinha)
+                return -1;
+
+            string titulo = html.Substring(inicioTitulo, fimTitulo - inicioTitulo).Trim();
+
+            data = System.Web.HttpUtility.HtmlDecode(data);
+            DateTime.TryParseExact(data, "dd/MM/yyyy", System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime d);
+
+            lista.Add(new Noticia()
+            {
+                Titulo = System.Web.HttpUtility.HtmlDecode(titulo),
+                DataStr = data,
+                Link = System.Web.HttpUtility.HtmlDecode(link),
+                Data = d
+            });
+
+            return fimLinha;
+        }
+
+        private List<Noticia> ParseNoticias(string html)
+        {
+            List<Noticia> lista = new List<Noticia>();
+
+            int inicio = 0;
+
+            do
+            {
+                inicio = html.IndexOf("<span class=\"icon-calendar\"></span>", inicio);
+                if (inicio > 0)
+                    inicio = ParseLinha(html, inicio + 35, lista);
+            } while (inicio > 0);
+
+            return lista;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            string executavel = GetType().Assembly.Location;
+
+            string valor = null;
+            try
+            {
+                valor = Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "HtmlDesktop", "") as string;
+            }
+            catch
+            {
+            }
+
+            if (executavel != valor)
+            {
+                try
+                {
+                    Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Run", "HtmlDesktop", executavel, Microsoft.Win32.RegistryValueKind.String);
+                }
+                catch
+                {
+                }
+            }
+
+
+            banco = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(executavel), "banco.bin");
+
+            noticias = new List<Noticia>();
+
+            using (var db = new LiteDB.LiteDatabase(banco))
+            {
+                var col = db.GetCollection<Noticia>();
+                noticias.AddRange(col.FindAll());
+            }
+
+            Atualizar();
+
+            notifyIcon.Icon = Icon;
+        }
+
+        private bool AnalisarNoticia(Noticia noticia)
+        {
+            string html;
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                html = System.Web.HttpUtility.HtmlDecode(client.DownloadString(noticia.Link));
+            }
+
+            
+
+            if (html.Contains("WACC"))
+            {
+                noticia.Notificacao = "WACC";
+                return true;
+            }
+            else if(html.Contains("Taxa de remuneração regulatória")|| html.Contains("taxa de remuneração regulatoria")|| html.Contains("taxa de remuneração") || html.Contains("taxa regulatória"))
+            {
+                noticia.Notificacao = "Taxa de remuneração regulatória";
+                return true;
+            }
+            else
+            {
+                noticia.Notificacao = "";
+                return false;
+            }            
+        }
+
+        private void Atualizar()
+        {
+            string html;
+            using (System.Net.WebClient client = new System.Net.WebClient())
+            {
+                html = client.DownloadString("https://www2.aneel.gov.br/aplicacoes_liferay/noticias_area/?idAreaNoticia=425&");                
+            }
+
+            List<Noticia> novasNoticias = ParseNoticias(html);
+
+            bool notificarBalao = false;
+
+            if (noticias != null)
+            {
+                foreach (Noticia nova in novasNoticias)
+                {
+                    bool analisar = true;
+                    foreach (Noticia velha in noticias)
+                    {
+                        if (velha.Titulo.Equals(nova.Titulo, StringComparison.OrdinalIgnoreCase))
+                        {
+                            nova.Notificacao = velha.Notificacao;
+                            analisar = false;
+                            break;
+                        }
+                    }
+                    nova.Analisar = analisar;
+                    if (analisar)
+                        notificarBalao |= AnalisarNoticia(nova);
+                }
+            }
+
+            noticias = novasNoticias;
+
+            listView.Items.Clear();
+
+            foreach (Noticia noticia in noticias)
+            {
+                if (noticia.Analisar)
+                    AnalisarNoticia(noticia);
+                var item = listView.Items.Add(noticia.Notificacao ?? "");
+                item.SubItems.Add(noticia.DataStr);
+                item.SubItems.Add(noticia.Titulo);
+                item.SubItems.Add(noticia.Link);
+            }
+
+            using (var db = new LiteDB.LiteDatabase(banco))
+            {
+                var col = db.GetCollection<Noticia>();
+                col.Delete(LiteDB.Query.All());
+                col.InsertBulk(noticias);
+            }
+
+            if (notificarBalao) {
+                notifyIcon.ShowBalloonTip(5000);
+                ExibirForm();
+            }
+        }
+
+        private void ExibirForm()
+        {
+            Visible = true;
+            if (WindowState == FormWindowState.Minimized)
+                WindowState = FormWindowState.Normal;
+            BringToFront();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            Atualizar();
+        }
+
+        private void listView_DoubleClick(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count == 0)
+                return;
+
+            Noticia noticia = noticias[listView.SelectedItems[0].Index];
+
+            SHELLEXECUTEINFO info = new SHELLEXECUTEINFO();
+            info.cbSize = Marshal.SizeOf(info);
+            info.lpVerb = "open";
+            info.lpFile = noticia.Link;
+            info.nShow = 3;
+            info.lpClass = "http";
+            ShellExecuteEx(ref info);
+            //MessageBox.Show(noticia.Link, "Oops...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void notifyIcon_Click(object sender, EventArgs e)
+        {
+            ExibirForm();
+        }
+
+        private void notifyIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            ExibirForm();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            bool mostrar = (WindowState == FormWindowState.Minimized);
+
+            if (mostrar == notifyIcon.Visible)
+                return;
+
+            notifyIcon.Visible = mostrar;
+            Visible = !mostrar;            
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Atualizar();
+        }
+    }
+}
